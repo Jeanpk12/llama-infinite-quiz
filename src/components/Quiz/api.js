69b98068b03bb3a8ea9f21ce2fe577eb78ce383c
Jean Oliveira <122842874+Jeanpk12@users.noreply.github.com>
2024-08-09 16:40:21 -0300

    Update api.js

--- src/components/Quiz/api.js
+++ src/components/Quiz/api.js
@@ -2,7 +2,7 @@ import Groq from "groq-sdk";
 
 // Use a API key diretamente com a opção de permitir o uso no navegador
 const groq = new Groq({
-  apiKey: "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",
+  apiKey: "API_KEY",
   dangerouslyAllowBrowser: true
 });
 
